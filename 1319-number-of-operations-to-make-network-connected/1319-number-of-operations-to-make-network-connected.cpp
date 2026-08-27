87b3447dc39dcdd9da2d0a3579be2d2d0cc97e5b
Ishit Kumar <ishitkumar3009@gmail.com>
2026-08-27 14:56:16 +0530

    Time: 22 ms (40.82%), Space: 47.3 MB (29.37%) - LeetHub

---
+++ 1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.cpp
@@ -0,0 +1,77 @@
+class DisjointSet {
+   public:
+    vector<int> rank;
+    vector<int> size;
+    vector<int> parent;
+
+    DisjointSet(int n) {
+        rank.resize(n + 1, 0);
+        parent.resize(n + 1);
+        size.resize(n + 1, 1);
+        for (int i = 0; i < n; i++) {
+            parent[i] = i;
+        }
+    }
+    int findUPar(int node) {
+        if (node == parent[node]) return node;
+        return parent[node] = findUPar(parent[node]);
+    }
+
+    bool find(int u, int v) { return (findUPar(u) == findUPar(v)); }
+
+    void unionByRank(int u, int v) {
+        int ulp_u = findUPar(u);
+        int ulp_v = findUPar(v);
+        if (ulp_u == ulp_v) return;
+        if (rank[ulp_u] < rank[ulp_v]) {
+            parent[ulp_u] = ulp_v;
+        } else if (rank[ulp_v] < rank[ulp_u]) {
+            parent[ulp_v] = ulp_u;
+        } else {
+            parent[ulp_v] = ulp_u;
+            rank[ulp_u]++;
+        }
+    }
+
+    void unionBySize(int u, int v) {
+        int ulp_u = findUPar(u);
+        int ulp_v = findUPar(v);
+
+        if (ulp_u == ulp_v) return;
+
+        if (size[ulp_u] < size[ulp_v]) {
+            parent[ulp_u] = ulp_v;
+            size[ulp_v] += size[ulp_u];
+        } else {
+            parent[ulp_v] = ulp_u;
+            size[ulp_u] += size[ulp_v];
+        }
+    }
+};
+
+class Solution {
+   public:
+    int makeConnected(int n, vector<vector<int>> &connections) {
+        DisjointSet ds(n);
+        int cntEx = 0;
+        for (auto it : connections) {
+            int u = it[0];
+            int v = it[1];
+            if (ds.findUPar(u) == ds.findUPar(v)) {
+                cntEx++;
+            } else
+                ds.unionBySize(u, v);
+        }
+        int cntC = 0;
+        for (int i = 0; i < n; i++) {
+            if (ds.parent[i] == i) {
+                cntC++;
+            }
+        }
+        int ans = cntC - 1;
+        if (cntEx >= ans)
+            return ans;
+        else
+            return -1;
+    }
+};
