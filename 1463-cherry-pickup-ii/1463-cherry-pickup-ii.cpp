1f8d31d29778d9ea2c7ed57b26a3684479158ec1
Ishit Kumar <ishitkumar3009@gmail.com>
2026-09-18 13:55:20 +0530

    Time: 75 ms (16.95%), Space: 20.9 MB (56.51%) - LeetHub

--- 1463-cherry-pickup-ii/1463-cherry-pickup-ii.cpp
+++ 1463-cherry-pickup-ii/1463-cherry-pickup-ii.cpp
@@ -1,46 +1,51 @@
 class Solution {
-   private:
-    int func(int i, int j1, int j2, int n, int m, vector<vector<int>> &grid,
-             vector<vector<vector<int>>> &dp) {
-        if (j1 < 0 || j1 >= m || j2 < 0 || j2 >= m) return -1e9;
+public:
+    int cherryPickup(vector<vector<int>>& grid) {
+        int n = grid.size();
+        int m = grid[0].size();
+        vector<vector<vector<int>>> dp(
+            n, vector<vector<int>>(m, vector<int>(m, 0)));
 
-        if (i == n - 1) {
-            if (j1 == j2)
-                return grid[i][j1];
-            else
-                return grid[i][j1] + grid[i][j2];
-        }
-
-        if (dp[i][j1][j2] != -1) return dp[i][j1][j2];
-
-        int maxi = INT_MIN;
-
-        for (int di = -1; di <= 1; di++) {
-            for (int dj = -1; dj <= 1; dj++) {
-                int ans;
-
-                if (j1 == j2)
-                    ans = grid[i][j1] +
-                          func(i + 1, j1 + di, j2 + dj, n, m, grid, dp);
-                else
-                    ans = grid[i][j1] + grid[i][j2] +
-                          func(i + 1, j1 + di, j2 + dj, n, m, grid, dp);
-
-                maxi = max(maxi, ans);
+        for (int j1 = 0; j1 < m; j1++) {
+            for (int j2 = 0; j2 < m; j2++) {
+                if (j1 == j2) {
+                    dp[n - 1][j1][j2] = grid[n - 1][j1];
+                } else {
+                    dp[n - 1][j1][j2] = grid[n - 1][j1] + grid[n - 1][j2];
+                }
             }
         }
 
-        return dp[i][j1][j2] = maxi;
-    }
+        for (int i = n - 2; i >= 0; i--) {
+            for (int j1 = 0; j1 < m; j1++) {
+                for (int j2 = 0; j2 < m; j2++) {
+                    int maxi = INT_MIN;
 
-   public:
-    int cherryPickup(vector<vector<int>> &grid) {
-        int n = grid.size();
-        int m = grid[0].size();
+                    for (int di = -1; di <= 1; di++) {
+                        for (int dj = -1; dj <= 1; dj++) {
+                            int newJ1 = j1 + di;
+                            int newJ2 = j2 + dj;
 
-        vector<vector<vector<int>>> dp(
-            n, vector<vector<int>>(m, vector<int>(m, -1)));
+                            if (newJ1 >= 0 && newJ1 < m && newJ2 >= 0 &&
+                                newJ2 < m) {
+                                int ans;
 
-        return func(0, 0, m - 1, n, m, grid, dp);
+                                if (j1 == j2) {
+                                    ans =
+                                        grid[i][j1] + dp[i + 1][newJ1][newJ2];
+                                } else {
+                                    ans = grid[i][j1] + grid[i][j2] +
+                                          dp[i + 1][newJ1][newJ2];
+                                }
+
+                                maxi = max(maxi, ans);
+                            }
+                        }
+                    }
+                    dp[i][j1][j2] = maxi;
+                }
+            }
+        }
+        return dp[0][0][m - 1];
     }
 };
